ed45d6683d75f96c0b37fb4c7ee0412b3abe0c0c
imkaran <karangalhotra0001@gmail.com>
2021-11-03 03:43:36 +0530

    compleated navbar UI

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 import './App.css';
-
+import Header from "./Header";
+import Home from "./Home";
 function App() {
   return (
     <div className="app">
-      <h1>Hello World, I'm building Amazon clone</h1>
-    
+    <Header/>
+    <Home/>
     </div>
   );
 }
---
+++ src/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./Home.css";
+function Home() {
+    return (
+        <div className="home">
+            <p>Karan start building home page without wasting any time</p>
+        </div>
+    )
+}
+
+export default Home
